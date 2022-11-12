a8c4868ece85a61d46ba0796c2b87d0dda673ee6
Newton <altontonnalumasa@gmail.com>
2022-11-12 19:35:32 +0300

    create list all rentals method per ID

--- app.rb
+++ app.rb
@@ -109,4 +109,12 @@ class App
     @rentals << rental
     puts 'Rental added successfully'
   end
+
+  def list_all_rentals 
+    puts 'Enter ID of a person'
+    id = gets.chomp.to_i
+    @rentals.each do |rental|
+      puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}" if rental.person.id == id;
+    end
+  end
 end
